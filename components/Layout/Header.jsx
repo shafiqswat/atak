a0b492577f52f411f98fed03b0075e7f113350b1
shafiqDev <shafiqswat02@gmail.com>
2024-09-22 16:18:43 +0300

    add smoth scroll

--- components/Layout/Header.jsx
+++ components/Layout/Header.jsx
@@ -1,8 +1,7 @@
 /** @format */
 "use client";
-
 import React, { useState } from "react";
-import Link from "next/link";
+import { Link } from "react-scroll";
 import CustomButton from "../Constants/Button";
 import { MenuIcon } from "../Constants/SvgIcons";
 import { useRouter } from "next/navigation";
@@ -25,7 +24,7 @@ const Header = () => {
         <div
           onClick={() => {
             router.push("/");
-            window.scrollTo({ top: 0, behavior: "smooth" });
+            window.scrollTo({ top: 0, behavior: "smooth" }); // Smooth scroll to top
           }}
           className='sm:px-5 md:px-14'>
           <img
@@ -47,26 +46,35 @@ const Header = () => {
         className={`px-2 pt-2 pb-4 sm:flex sm:items-center sm:p-0 ${
           isOpen ? "block" : "hidden"
         }`}>
+        {/* Use react-scroll's Link for smooth scroll */}
         <Link
-          href='#hardware'
-          className='block px-10 sm:px-3 py-1'
+          to='hardware'
+          smooth={true}
+          duration={1000}
+          className='block px-10 sm:px-3 py-1 cursor-pointer'
           onClick={closeMenu}>
           Hardware
         </Link>
         <Link
-          href='#services'
-          className='sm:mt-0 sm:ml-2 block px-10 sm:px-3 py-1'
+          to='services'
+          smooth={true}
+          duration={1500}
+          className='sm:mt-0 sm:ml-2 block px-10 sm:px-3 py-1 cursor-pointer'
           onClick={closeMenu}>
           Services
         </Link>
         <Link
-          href='#training'
-          className='sm:mt-0 sm:ml-2 block px-10 sm:px-3 py-1'
+          to='training'
+          smooth={true}
+          duration={2000}
+          className='sm:mt-0 sm:ml-2 block px-10 sm:px-3 py-1 cursor-pointer'
           onClick={closeMenu}>
           Training
         </Link>
         <Link
-          href='#contact'
+          to='contact'
+          smooth={true}
+          duration={800}
           className='sm:mt-0 sm:ml-2 block px-10 sm:px-5 md:px-10 py-1'
           onClick={closeMenu}>
           <CustomButton />
